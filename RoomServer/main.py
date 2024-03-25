a1b62efec91af02f268e2cd4b987c04deaf5ea1c
Claudio Ciano <shapelessguy@gmail.com>
2024-03-25 16:57:01 +0100

    upd4

--- RoomServer/main.py
+++ RoomServer/main.py
@@ -173,7 +173,7 @@ def actuator(active_times, commands):
                     if command == 'AUDIOON':
                         reply = f'Command {command} sent'
                         write('AUDIOON/OFF')
-                        time.sleep(1000)
+                        time.sleep(500)
                         write('AUDIOON/OFF')
                         print(reply)
                     else:
