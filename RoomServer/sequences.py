c5fd9be1e63d09cb7087397cb73279028835bc3a
Claudio Ciano <shapelessguy@gmail.com>
2024-05-09 22:32:37 +0200

    strip try2

--- RoomServer/sequences.py
+++ RoomServer/sequences.py
@@ -20,58 +20,58 @@ def audio_off(write: callable):
 def handle_strip_com(command, write: callable):
     if command == 'TOPLIGHT':
         write('STRIPOFF')
-        time.sleep(50)
+        time.sleep(0.50)
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         write('STRIPTOPCOLSWITCH')
-        time.sleep(100)
+        time.sleep(0.100)
         write('STRIPTOPLIGHTSWITCH')
-        time.sleep(100)
+        time.sleep(0.100)
         write('STRIPON')
-        time.sleep(100)
+        time.sleep(0.100)
         write('STRIPW0')
     elif command == 'TOPB3':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         for _ in range(1):
             write('STRIPTOPCHANGECOL')
-            time.sleep(100)
+            time.sleep(0.100)
         write('STRIPB3')
     elif command == 'TOPR0':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         for _ in range(2):
             write('STRIPTOPCHANGECOL')
-            time.sleep(100)
+            time.sleep(0.100)
         write('STRIPR0')
     elif command == 'TOPG0':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         for _ in range(3):
             write('STRIPTOPCHANGECOL')
-            time.sleep(100)
+            time.sleep(0.100)
         write('STRIPG0')
     elif command == 'TOPB0':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         for _ in range(4):
             write('STRIPTOPCHANGECOL')
-            time.sleep(100)
+            time.sleep(0.100)
         write('STRIPB0')
     elif command == 'TOPR4':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         for _ in range(5):
             write('STRIPTOPCHANGECOL')
-            time.sleep(100)
+            time.sleep(0.100)
         write('STRIPR4')
     elif command == 'TOPG4':
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         write('STRIPG4')
     elif command == 'TOPOFF':
         write('STRIPOFF')
-        time.sleep(50)
+        time.sleep(0.50)
         write('STRIPTOPDEF')
-        time.sleep(250)
+        time.sleep(0.250)
         write('STRIPTOPCOLSWITCH')
