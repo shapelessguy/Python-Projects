819952c701c47dc779e1b77e4b3ca18017aadc13
cian_cl <claudio.ciano@dlr.de>
2025-10-05 16:44:10 +0200

    auto_update

--- RoomServer/wg/bac.py
+++ RoomServer/wg/bac.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(__file__))
 from bac_utils import *
 from variables import members, nominal_activities
 import datetime
+import random
 
 
 class Activity:
@@ -332,8 +333,7 @@ def assign_tasks(wg_members: WgMembers, avail_members: list[WgMember]):
 
     best_fitness = min([x[-1] for x in all_state_options])
     best_candidates = [x[0] for x in all_state_options if x[-1] == best_fitness]
-    print(len(best_candidates))
-    best = min(all_state_options, key=lambda x: x[1])[0]
+    best = random.choice(best_candidates)
     for m, a in best.items():
         m.assign(a)
 
