8d23ded4f4d5e65a0e3971d787e7d3f90fb56ffd
Claudio <claudiociano42@gmail.com>
2025-12-22 10:59:11 +0100

    auto_update

--- RoomServer/announcements.py
+++ RoomServer/announcements.py
@@ -75,8 +75,8 @@ def set_announcement(updated=False):
             blames, blamed_activity = get_blame(name, str(blame_first_date), hist_df, logs)
             telegram_id = e['telegram_id']
             string = '<b>🔄 PLAN UPDATED DURING THIS WEEK</b>\n' if updated else ''
-            string += f'Hello {name}, this is the schedule for the next weeks, waiting for you! 🤩\n'
-            string += 'Keep in mind that this is just a preview on your next activities.. things may change!\n'
+            string += f'Hey {name}, this is the schedule for the next weeks, waiting for you! 🤩\n'
+            string += 'Keep in mind that this is just a preview on your next activities.. things may change!\n\n'
             for week_n, week_tasks in enumerate(week_schedule):
                 week_now = (now + timedelta(days=(week_n * 7))).date().strftime("%d/%m")
                 week_plus_1 = (now + timedelta(days=((week_n + 1) * 7))).date().strftime("%d/%m")
