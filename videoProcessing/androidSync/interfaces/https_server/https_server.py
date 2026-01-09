23356c6bfb4a729c886c431d9de0bfef278fa40f
Claudio <shapelessguy@gmail.com>
2026-01-09 21:54:20 +0100

    max-size = None

--- videoProcessing/androidSync/interfaces/https_server/https_server.py
+++ videoProcessing/androidSync/interfaces/https_server/https_server.py
@@ -32,6 +32,15 @@ async def run_server(signal_):
     global signal
     signal = signal_
     stop_event = signal["kill_server"]
-    async with websockets.serve(handle, "0.0.0.0", PORT, ssl=ssl_context):
+    async with websockets.serve(
+        handle,
+        "0.0.0.0",
+        PORT,
+        ssl=ssl_context,
+        max_size=None,
+        max_queue=None,
+        read_limit=2**20,
+        write_limit=2**20
+    ):
         print(f"WSS server listening on port {PORT}")
         await stop_event.wait()
