af34e6f1411046cd428bdf6c90678a73b47c9883
Claudio <shapelessguy@gmail.com>
2026-02-19 08:36:38 +0100

    cyan app launcher

---
+++ CyanManager/tools/CyanAppLauncher/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CyanAdminLauncher
+{
+    internal class Program
+    {
+        private static string appGuid = "c0a76b5a-12ab-45c5-b9d9-d693faa6e9b9";
+        private const string REG_PATH = @"SOFTWARE\CyanTools\Launcher";
+        private const string REG_VALUE_NAME = "LaunchPending";
+        static void Main(string[] args)
+        {
+            string exePath = Assembly.GetEntryAssembly().Location;
+            bool createdNew;
+            new Mutex(true, "Global\\" + appGuid, out createdNew);
+            if (!createdNew)
+            {
+                Console.WriteLine("Another instance is already running.");
+                return;
+            }
+
+
+            while (true)
+            {
+                try
+                {
+                    using (RegistryKey key = Registry.CurrentUser.OpenSubKey(REG_PATH))
+                    {
+                        if (key != null)
+                        {
+                            object value = key.GetValue(REG_VALUE_NAME);
+                            if (value != null && value.ToString() != "")
+                            {
+                                LaunchTarget(value.ToString());
+                                using (RegistryKey writeKey = Registry.CurrentUser.OpenSubKey(REG_PATH, true))
+                                {
+                                    if (writeKey != null) writeKey.DeleteValue(REG_VALUE_NAME, false);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Registry poll error: " + ex.Message);
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void LaunchTarget(string exePath)
+        {
+            if (!File.Exists(exePath))
+            {
+                Console.WriteLine("Target not found: " + exePath);
+                return;
+            }
+
+            Console.WriteLine("Launching target: " + exePath);
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                UseShellExecute = true,
+                WorkingDirectory = Path.GetDirectoryName(exePath),
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+
+            try
+            {
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Launch failed: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ CyanManager/tools/CyanAppLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CyanAppLauncher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CyanAppLauncher")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5629f676-66a7-42a9-a076-82ecfd325cc6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
