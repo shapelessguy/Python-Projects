01daaba3984688a0d02272e482542c431d3052ac
Claudio <shapelessguy@gmail.com>
2026-03-07 11:19:49 +0100

    small changes

--- e-cig/cases/create_base.py
+++ e-cig/cases/create_base.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from resources import *
 
-TOTAL_VOLUME_ML = 250
+TOTAL_VOLUME_ML = 1000
 
 base: Mixture = default_base.liquid.compose(name=f"Base", ref={'tot': TOTAL_VOLUME_ML}, show=True)
